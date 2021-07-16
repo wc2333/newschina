5f9fdaf426254986e22cc371a4559379d4056113
wc2333 <2418788460@qq.com>
2021-07-16 09:49:54 +0800

    7.16

---
+++ newschina/src/main/java/com/zd/NewschinaApplication.java
@@ -0,0 +1,13 @@
+package com.zd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewschinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewschinaApplication.class, args);
+    }
+
+}
---
+++ newschina/src/test/java/com/example/newschina/NewschinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newschina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewschinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
